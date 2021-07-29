075f4832582780e143ee2e95cc471f7da5a5d8e7
Aliaksei Anikiyenka <aliaksei_anikiyenka@epam.com>
2021-07-29 10:50:17 +0300

    Fix linter warnings

--- codeartifact_cleanup.py
+++ codeartifact_cleanup.py
@@ -1,6 +1,7 @@
-import boto3
 import os
+import boto3
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -18,16 +19,17 @@ client = boto3.client('codeartifact')
 # client = session.client('codeartifact')
 
 def list_packages(domain, domainOwner, repository, format):
+    """Get list packages in CodeArtifact repository"""
     response = client.list_packages(
         domain=domain,
         domainOwner=domainOwner,
         repository=repository,
         format=format,
         maxResults=100)
-
     return response['packages']
 
 def list_package_versions(domain, domainOwner, repository, format, namespace, package):
+    """Get package versions in CodeArtifact repository"""
     response = client.list_package_versions(
         domain=domain,
         domainOwner=domainOwner,
@@ -39,10 +41,10 @@ def list_package_versions(domain, domainOwner, repository, format, namespace, pa
         sortBy='PUBLISHED_TIME',
         maxResults=100
     )
-
     return response["versions"]
 
-def delete_package_versions(domain, domainOwner, repository, format, namespace, package, list):
+def delete_package_versions(domain, domainOwner, repository, format, namespace, package, list_of_versions):
+    """Delete package versions from CodeArtifact repository"""
     response = client.delete_package_versions(
         domain=domain,
         domainOwner=domainOwner,
@@ -50,21 +52,35 @@ def delete_package_versions(domain, domainOwner, repository, format, namespace, 
         format=format,
         namespace=namespace,
         package=package,
-        versions=list,
+        versions=list_of_versions,
         expectedStatus=artifact_status_to_delete
     )
     return response
 
-def create_list_versions(list):
-    list_versions = [item['version'] for item in list]
-
+def create_list_versions(list_of_versions):
+    """Create a list of package versions available in CodeArtifact repository"""
+    list_versions = [item['version'] for item in list_of_versions]
     return list_versions
 
-for pkg in list_packages(domain_name, domain_owner, repository_name, format_artifact):
-    list_art_vers = list_package_versions(domain_name, domain_owner, repository_name, format_artifact, pkg['namespace'], pkg['package'])
+for pkg in list_packages(domain_name,
+                            domain_owner,
+                            repository_name,
+                            format_artifact):
+    list_art_vers = list_package_versions(domain_name,
+                                            domain_owner,
+                                            repository_name,
+                                            format_artifact,
+                                            pkg['namespace'],
+                                            pkg['package'])
     if len(list_art_vers) > 0:
         versions = create_list_versions(list_art_vers)
         print(f'Packages will be deleted: {pkg["namespace"]}:{pkg["package"]}: {versions}')
-        delete_package_versions(domain_name, domain_owner, repository_name, format_artifact, pkg['namespace'], pkg['package'], versions)
+        delete_package_versions(domain_name,
+                                domain_owner,
+                                repository_name,
+                                format_artifact,
+                                pkg['namespace'],
+                                pkg['package'],
+                                versions)
     else:
         print(f'Nothing to delete for package: {pkg["namespace"]}:{pkg["package"]}')
